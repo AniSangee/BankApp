ce5a5e86b7ad2ce5f0aa94133cf082974e14fc29
AniSangee <anilshanthipura547@gmail.com>
2024-11-09 19:16:41 +0530

    Add

--- bank/homepage/models.py
+++ bank/homepage/models.py
@@ -15,3 +15,4 @@ class Transaction(models.Model):
     transaction_type = models.CharField(max_length=10, choices=TRANSACTION_TYPES)
     amount = models.DecimalField(max_digits=10, decimal_places=2)
     created_at = models.DateTimeField(auto_now_add=True)
+
